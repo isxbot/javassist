d500658ddde22b70b646833e4534f279e752a9fc
Dillon Isaacs <isxbot@gmail.com>
2020-02-04 15:52:50 -0600

    Start implementation of ex0130

--- javassist-project-isaacs-0130/src/ex0130/CustomClassLoader.java
+++ javassist-project-isaacs-0130/src/ex0130/CustomClassLoader.java
@@ -1,15 +1,31 @@
 package ex0130;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Modifier;
 import java.util.Scanner;
 
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.NotFoundException;
+
 public class CustomClassLoader extends java.lang.ClassLoader {
+	static String WORK_DIR = System.getProperty("user.dir");
+	static String INPUT_DIR = WORK_DIR + File.separator + "bin";
 	Scanner input = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		String[] classAndField = new String[2];
+		CustomClassLoader loader = new CustomClassLoader();
+		
+		// Get class and field for modification.
+		loader.getInput(classAndField);
 		
 		// Find class
-		Class<?> foundClass = new CustomClassLoader().findClass(classAndField[0]);
+		Class<?> foundClass = loader.findClass(classAndField);
+		foundClass.getDeclaredMethod("main", new Class[] { String[].class }).invoke(null, new Object[] { args });
 		
 		// Add field
 
@@ -18,19 +34,33 @@ public class CustomClassLoader extends java.lang.ClassLoader {
 	/*
 	 * Find class matching given String class name.
 	 */
-	@Override
-	public Class findClass(String name) {
-		return null;
-		
+	public Class<?> findClass(String[] classAndField) throws ClassNotFoundException {
+		try {
+			ClassPool pool = new ClassPool();
+			CtClass cc = pool.get("target." + classAndField[0]);
+			if(classAndField[0].contentEquals("ComponentApp") | classAndField[0].contentEquals("ServiceApp")) {
+				CtField field = new CtField(CtClass.doubleType, classAndField[1], cc);
+				field.setModifiers(Modifier.PUBLIC);
+				cc.addField(field, CtField.Initializer.constant(0));
+			}
+			byte[] bArr = cc.toBytecode();
+			return defineClass(classAndField[0], bArr, 0, bArr.length);
+		} catch (NotFoundException e) {
+	         throw new ClassNotFoundException();
+	      } catch (IOException e) {
+	         throw new ClassNotFoundException();
+	      } catch (CannotCompileException e) {
+	         throw new ClassNotFoundException();
+	      }
 	}
 	
 	/*
 	 * Add a field to given class.
 	 */
-	Class addField(String field, Class<?> clazz) {
-		return clazz;
-		
-	}
+//	Class addField(String field, Class<?> clazz) {
+//		return clazz;
+//		
+//	}
 	
 	void getInput(String[] classAndField) {
 		System.out.println("Enter a class name: ");
