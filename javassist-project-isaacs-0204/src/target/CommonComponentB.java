c94e4770700eb9cca3e2300bc3e32fdb23b6acd3
Dillon Isaacs <isxbot@gmail.com>
2020-02-04 16:39:49 -0600

    Initial creation of exam project.

---
+++ javassist-project-isaacs-0204/src/target/CommonComponentB.java
@@ -0,0 +1,11 @@
+package target;
+
+public class CommonComponentB {
+    int idB = 0;
+    String nameB = "";
+    
+    public CommonComponentB() {
+    	idB = 83439324;
+    	nameB = "isaacs";
+    }
+}
---
+++ javassist-project-isaacs-0204/src/target/CommonServiceA.java
@@ -0,0 +1,11 @@
+package target;
+
+public class CommonServiceA {
+    int idA = 0;
+    String nameA = "";
+    
+    public CommonServiceA() {
+    	idA = 83439324;
+    	nameA = "isaacs";
+    }
+}
