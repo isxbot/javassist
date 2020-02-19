831518d248440a64591c80d70ce3273cc23be65d
Dillon Isaacs <isxbot@gmail.com>
2020-02-19 17:18:17 -0600

    Initial creation of 0218.

---
+++ javassist-project-isaacs-tse-0218/src/AnnotatedFieldExample.java
@@ -0,0 +1,14 @@
+import util.UtilMenu;
+
+public class AnnotatedFieldExample {
+
+	public static void main(String[] args) {
+		
+		// Get user input.
+		UtilMenu.showMenuOptions();
+		String[] arguments = UtilMenu.getArguments();
+		UtilMenu.getArguments();
+
+	}
+
+}
